a01e0436a3903fe3e28e4bccd0bd730027f1e771
Luis <LVentura335@Gmail.com>
2020-10-16 23:14:35 -0700

    minor syntax revisions

--- resources/js/data.js
+++ resources/js/data.js
@@ -1,13 +1,11 @@
 export class Unit {
-    constructor(name, maxHP, maxEN, hp, en, attacks) {
+    constructor(name, maxHP, maxEN, hp, en, attacks, attackPotency) {
         this.name = name
         this.maxHP = maxHP
         this.maxEN = maxEN
         this.hp = hp
         this.en = en
         this.attacks = attacks
-    }
-    getName(){
-        return this.name
+        this.attackPotency = attackPotency
     }
 }
--- resources/js/script.js
+++ resources/js/script.js
@@ -3,23 +3,29 @@ import {Unit} from './data.js'
 window.addEventListener('load', initializeStats)
 
 
-let playerUnit = new Unit("Zack", 5600, 200, 5600, 200, ["O Shot", "O Strike", "O Finish"])
-console.log(playerUnit)
+let playerUnit = new Unit("Zack", 5600, 200, 5600, 200,
+["O Shot", "O Strike", "O Finish"], 
+[2500, 2800, 3500])
 
-const playerHealth = document.getElementById('player-hp')
-const playerEnergy = document.getElementById('player-en')
 
-const enemyHealth = document.getElementById('enemy-hp')
-const enemyEnergy = document.getElementById('enemy-en')
+let enemyUnit = new Unit("Gunguy", 12000, 450, 12000, 450,
+["Blast Shot", "Collision"],
+[2500, 4000])
 
-const playerAction = document.getElementById('action-output')
-playerAction.addEventListener('click', changeAction)
+const playerHealth = document.getElementById("player-hp")
+const playerEnergy = document.getElementById("player-en")
 
-const playerAttack = document.getElementById('attack-output')
-playerAttack.addEventListener('click', changeAttack)
+const enemyHealth = document.getElementById("enemy-hp")
+const enemyEnergy = document.getElementById("enemy-en")
 
-const combatStart = document.getElementById('final-confirm')
-combatStart.addEventListener('click', fightProcess)
+const playerAction = document.getElementById("action-output")
+playerAction.addEventListener("click", changeAction)
+
+const playerAttack = document.getElementById("attack-output")
+playerAttack.addEventListener("click", changeAttack)
+
+const combatStart = document.getElementById("final-confirm")
+combatStart.addEventListener("click", fightProcess)
 
 function initializeStats() {
     let playerHP = playerUnit.hp
@@ -27,33 +33,49 @@ function initializeStats() {
 
     let playerEN = playerUnit.en
     playerEnergy.textContent += playerEN
+
+    let enemyHP = enemyUnit.hp
+    enemyHealth.textContent += enemyHP
+
+    let enemyEN = enemyUnit.en
+    enemyEnergy.textContent += enemyEN
+
+    changeAttack()
 }
 
 function changeAction() {
     switch(playerAction.innerText){
-        case 'Attack':
-            playerAction.innerText = 'Defend'
+        case "Attack":
+            playerAction.innerText = "Defend"
             break;
         case 'Defend':
-            playerAction.innerText = 'Evade'
+            playerAction.innerText = "Evade"
             break;
         case 'Evade':
-            playerAction.innerText = 'Attack'
+            playerAction.innerText = "Attack"
             break;
     }
     changeAttack()
 }
 
 function changeAttack() {
-    if (playerAction.innerText == 'Attack') {
+    if (playerAction.innerText == "Attack") {
         const attackList = playerUnit.attacks
-        console.log(attackList)
+        let selectedAttack = playerAttack.innerText
+        let nextAttack = attackList.indexOf(selectedAttack) + 1
+        if (playerAttack.innerText == "---" 
+        || attackList[nextAttack] == attackList[attackList.length]) {
+            playerAttack.innerText = attackList[0]
+        }
+        else {
+            playerAttack.innerText = attackList[nextAttack]
+        }
     }
     else {
-        playerAttack.innerText = '---'
+        playerAttack.innerText = "---"
     }
 }
 
 function fightProcess() {
-    console.log(data.playerData.Attacks)
+    console.log(playerUnit.attacks)
 }
