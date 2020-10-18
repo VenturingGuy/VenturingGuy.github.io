b28197899d24728c42447a5eac51a3cbc0c8b099
Luis <LVentura335@Gmail.com>
2020-10-18 12:34:28 -0700

    made further adjustments to data.js values and functions

--- resources/js/data.js
+++ resources/js/data.js
@@ -2,83 +2,30 @@ export class Unit {
     /* So this kinda got out of hand. Learning experience. */
     constructor(params) {
         this.stats = params
-        let terrainPerformace = this.getTerrainPerformace
-        this.terrainPerformance = terrainPerformace
-        let totalPerformance = this.getTotalPerformance
-        this.totalPerformance = totalPerformance
-    }
-
-    get getTerrainPerformace() {
-        return this.calcTerrainPerformace()
-    }
-
-    get getTotalPerformance() {
-        return this.calcTotalPerformance()
+        this.stats.terrainPerformance = this.terrainMap[this.stats.pilotPerformance]
+        + this.terrainMap[this.stats.unitPerformance]
+        this.stats.totalPerformance = this.totalMap[this.stats.terrainPerformance]
+        this.stats.sizeAdjustment = this.sizeMap[this.stats.unitSize]
     }
     
-    calcTerrainPerformace() {
-        let pilotNum = 3
-        let unitNum = 3
-        switch(this.pilotPerformance){
-            case "S":
-                pilotNum = 4
-                break;
-            case "A":
-                pilotNum = 3
-                break;
-            case "B":
-                pilotNum = 2
-                break;
-            case "C":
-                pilotNum = 1
-                break;
-            case "D":
-                pilotNum = 0
-                break;
-        }
-        switch(this.unitPerformance){
-            case "S":
-                unitNum = 4
-                break;
-            case "A":
-                unitNum = 3
-                break;
-            case "B":
-                unitNum = 2
-                break;
-            case "C":
-                unitNum = 1
-                break;
-            case "D":
-                unitNum = 0
-                break;
-        }
-        return pilotNum + unitNum
-
+    terrainMap = {
+        S: 4,
+        A: 3,
+        B: 2,
+        C: 1,
+        D: 0
     }
 
-    calcTotalPerformance() {
-        let totalAdjustment = 1.0
-        if (this.terrainPerformance > 6){
-            totalAdjustment = 1.1
-        }
-        else if (this.terrainPerformance > 5){
-            totalAdjustment = 1.0
-        }
-        else if (this.terrainPerformance > 3){
-            totalAdjustment = 0.9
-        }
-        else if (this.terrainPerformance > 1){
-            totalAdjustment = 0.8
-        }
-        else{
-            totalAdjustment = 0.4
-        }
-        return totalAdjustment
-    }
-
-    get getSizeAdjustment() {
-        return this.sizeMap[this.unitSize]
+    totalMap = {
+        8: 1.1,
+        7: 1.1,
+        6: 1.0,
+        5: 0.9,
+        4: 0.9,
+        3: 0.8,
+        2: 0.8,
+        1: 0.4,
+        0: 0.4
     }
 
     sizeMap = {
